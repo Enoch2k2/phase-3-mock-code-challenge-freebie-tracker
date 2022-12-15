32b35c2c18045bcd62bf65aab537c1c6d101f833
Enoch2k2 <enoch2k2@hotmail.com>
2022-12-15 14:43:10 -0600

    finished project

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,16 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+#   Company#give_freebie(dev, item_name, value)
+
+  def give_freebie(dev, item_name, value)
+    # takes a dev (an instance of the Dev class), an item_name (string), and a value as arguments, and creates a new Freebie instance associated with this company and the given dev
+    self.freebies.create(dev: dev, item_name: item_name, value: value)
+  end
+
+  def self.oldest_company
+    # we need to get the oldest company by founding_year
+    self.order(founding_year: :asc)[0]
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,24 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  #received_one?(item_name)
+  def received_one?(item_name)
+    # accepts an item_name (string) and returns true if any of the freebies associated with the dev has that item_name, otherwise returns false
+    # binding.pry
+    # self.freebies.where(item_name: item_name).length > 0
+    !self.freebies.where(item_name: item_name).empty?
+  end
+
+
+
+  # give_away(dev, freebie)
+  
+  def give_away(dev, freebie)
+    # accepts a Dev instance and a Freebie instance, changes the freebie's dev to be the given dev; your code should only make the change if the freebie belongs to the dev who's giving it away
+
+    if freebie.dev.id == self.id
+      freebie.update(dev: dev)
+    end
+  end
 end
